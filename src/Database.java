915418abe05cfca31ccae5242ccf90bdf9be3647
gravitygo <chyle.andrei.lee@gmail.com>
2023-07-22 10:56:59 +0800

    Finished the program: Need ERROR CHECKING

--- src/Database.java
+++ src/Database.java
@@ -6,42 +6,22 @@ public class Database {
     private String username = "root";
     private String password = "password";
 
-    private void connect(){
+    public Connection getConn(){
         try {
             conn = DriverManager.getConnection(dbURL, username, password);
-            System.out.println("Connected to the database.");
-        } catch (
-                SQLException e) {
+            conn.setAutoCommit(false);
+            return conn;
+        } catch (SQLException e) {
             System.out.println("Connection failed. Error: " + e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println("Error in closing the connection: " + ex.getMessage());
-            }
+            return conn;
         }
     }
-    public Connection getConnection(){
-        return conn;
+
+    public void closeConn() throws SQLException {
+        conn.close();
     }
 
-    //TODO: Not suree abt this yet*
-    public ResultSet useQuery(String query, String[] values) throws SQLException {
-        connect();
-        conn.setAutoCommit(false);
-
-        PreparedStatement pstmt = conn.prepareStatement(query);
-
-        for (int i = 0; i < values.length; i++)
-            pstmt.setString(i + 1, values[i]);
-
-        ResultSet rs = pstmt.executeQuery();
-
-        pstmt.close();
+    public void commit() throws SQLException {
         conn.commit();
-        conn.close();
-        return rs;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+    Database db;
+    Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws SQLException {
         Main m = new Main();
+        m.db = new Database();
         int menu;
         while(true){
             System.out.println("Menu");
@@ -17,9 +18,7 @@ public class Main {
             System.out.println("4.) Cancel Order");
 
             System.out.print("Select an Option: ");
-            menu= s.nextInt();
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
+            menu= m.s.nextInt();
             switch(menu){
                 case 1:
                     m.createOrder();
@@ -40,20 +39,313 @@ public class Main {
         }
     }
     //TODO: Chyle
-    public void createOrder(){
-        System.out.println("1.) Create and Order");
+    public void createOrder() throws SQLException {
+        s.nextLine();
+        System.out.print("Enter Customer Number: ");
+        String customerNumber = s.nextLine();
+        System.out.print("Enter Required Date: ");
+        String requiredDate = s.nextLine();
+        Connection conn = db.getConn();
+
+        String query = " INSERT INTO orders(" +
+                "       orderDate" +
+                "     , requiredDate" +
+                "     , status" +
+                "     , customerNumber)" +
+                " VALUES (" +
+                "       NOW()" +
+                "     , ?" +
+                "     , 'In Process'" +
+                "     , ?" +
+                " );";
+        String queryProducts = " INSERT INTO orderdetails VALUES";
+        ArrayList<String> updates = new ArrayList<String>();
+        String updateQuery="";
+
+        boolean addProduct = true;
+        int i=1;
+        do{
+            System.out.print("Enter Product Code: ");
+            String productCode = s.nextLine();
+            System.out.print("Enter Quantity: ");
+            int quantity = s.nextInt();
+            System.out.print("Enter Price: ");
+            float price = s.nextFloat();
+            s.nextLine();
+            queryProducts += " (? " +
+                             " , '" + productCode + "'" +
+                             " , " + quantity +
+                             " , " + price +
+                             " , " + i +
+                             " )";
+            System.out.println("[Input 'y' if you want to add another product]");
+            String yes = s.nextLine();
+            if(yes.charAt(0)=='y') queryProducts += ", ";
+            else addProduct = false;
+            i++;
+
+            updateQuery =  " UPDATE products SET" +
+                            "      quantityInStock = quantityInStock - " + quantity + "" +
+                            " WHERE productCode = '" + productCode + "';";
+
+            updates.add(updateQuery);
+        }while(addProduct);
+        queryProducts += ";";
+        PreparedStatement pstmtOrder = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        pstmtOrder.setString(1, requiredDate);
+        pstmtOrder.setString(2, customerNumber);
+
+        System.out.println("[Press ENTER key to create Order]");
+        s.nextLine();
+
+
+        pstmtOrder.executeUpdate();
+        int orderNumber = 0;
+
+        ResultSet rs = pstmtOrder.getGeneratedKeys();
+        if (rs.next())
+            orderNumber = rs.getInt(1);
+        rs.close();
+
+        PreparedStatement pstmtOrderDetail = conn.prepareStatement(queryProducts);
+        for(int j = i-1; j>0; j--)
+            pstmtOrderDetail.setInt(j, orderNumber);
+
+        System.out.println("[Press ENTER key to create Order Details]");
+        s.nextLine();
+        pstmtOrderDetail.executeUpdate();
+
+        System.out.println("[Press ENTER key to update Quantity]");
+        for(String uQ : updates){
+            PreparedStatement pstmtUpdate = conn.prepareStatement(uQ);
+            pstmtUpdate.executeUpdate();
+
+            pstmtUpdate.close();
+        }
+
+        db.commit();
+        System.out.println("[Press ENTER key to end Transaction]");
+        s.nextLine();
+
+        pstmtOrder.close();
+        pstmtOrderDetail.close();
+
+        db.closeConn();
     }
+
+
     //TODO
-    public void inquireProducts(){
-        System.out.println("2.) Inquire products");
+    public void inquireProducts() throws SQLException {
+        System.out.print("Enter Product Code: ");
+        String productCode = s.next();
+        s.nextLine();
+        Connection conn = db.getConn();
+        String query =  "SELECT" +
+                        "     productName" +
+                        "     , productLine" +
+                        "     , quantityInStock" +
+                        "     , buyPrice" +
+                        "     , MSRP" +
+                        " FROM products" +
+                        " WHERE productCode = ?" +
+                        " LOCK IN SHARE MODE";
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, productCode);
+
+        System.out.println("[Press ENTER key to start retrieving the data]");
+        s.nextLine();
+
+        ResultSet rs = pstmt.executeQuery();
+
+        String productName = "", productLine = "";
+        int quantityInStock = 0;
+        float buyPrice = 0, MSRP = 0;
+
+        while (rs.next()) {
+            productName     = rs.getString("productName");
+            productLine     = rs.getString("productLine");
+            quantityInStock = rs.getInt("quantityInStock");
+            buyPrice        = rs.getFloat("buyPrice");
+            MSRP            = rs.getFloat("MSRP");
+        }
+
+        rs.close();
+
+        System.out.println("Product Name: " + productName);
+        System.out.println("Product Line: " + productLine);
+        System.out.println("Quantity:     " + quantityInStock);
+        System.out.println("Buy Price:    " + buyPrice);
+        System.out.println("MSRP:         " + MSRP);
+
+        db.commit();
+        System.out.println("[Press ENTER key to end Transaction]");
+        s.nextLine();
+
+        pstmt.close();
+
+        db.closeConn();
     }
+
     //TODO
-    public void getOrderInfo(){
-        System.out.println("3.) Retrieve order info");
+    public void getOrderInfo() throws SQLException {
+        System.out.print("Enter Order ID: ");
+        String orderID = s.next();
+        s.nextLine();
+        Connection conn = db.getConn();
+        String query =  " SELECT " +
+                        "     orderDate" +
+                        "     , requiredDate" +
+                        "     , shippedDate" +
+                        "     , customerName" +
+                        "     , status" +
+                        "     , productName" +
+                        "     , quantityOrdered" +
+                        "     , priceEach" +
+                        "     , orderLineNumber " +
+                        " FROM orders o " +
+                        " LEFT JOIN orderdetails od ON od.orderNumber   = o.orderNumber" +
+                        " LEFT JOIN products      p ON od.productCode   = p.productCode" +
+                        " LEFT JOIN customers     c ON c.customerNumber = o.customerNumber" +
+                        " WHERE o.orderNumber = ?" +
+                        " LOCK IN SHARE MODE";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, orderID);
+
+        System.out.println("[Press ENTER key to start retrieving the data]");
+        s.nextLine();
+
+        ResultSet rs = pstmt.executeQuery();
+
+        boolean first = true;
+
+        while (rs.next()) {
+            if(first){
+                System.out.println("Order Information");
+                System.out.println("=====================");
+                System.out.println("Order Date:    " + rs.getDate("orderDate"));
+                System.out.println("Required Date: " + rs.getDate("requiredDate"));
+                System.out.println("Status:        " + rs.getString("status"));
+                Date shippedDate = rs.getDate("shippedDate");
+
+                if(shippedDate!=null)
+                    System.out.println("Shipped Date:  " + shippedDate);
+
+                System.out.println("Customer Name: " + rs.getString("customerName"));
+                first = false;
+
+                System.out.println();
+                System.out.println("Order Details");
+                System.out.println("=====================");
+                System.out.println();
+            }
+            System.out.println(rs.getString("productName"));
+            System.out.println("=====================");
+            System.out.println("Quantity:  " + rs.getInt("quantityOrdered"));
+            System.out.println("Individual Price:  " + rs.getFloat("priceEach"));
+            System.out.println("Order Line Number: " + rs.getInt("orderLineNumber"));
+            System.out.println();
+        }
+        rs.close();
+
+        db.commit();
+        System.out.println("[Press ENTER key to end Transaction]");
+        s.nextLine();
+        pstmt.close();
+        db.closeConn();
     }
+
     //TODO
-    public void cancelOrder(){
-        System.out.println("4.) Cancel Order");
+    public void cancelOrder() throws SQLException {
+        System.out.print("Enter Order ID: ");
+        String orderID = s.next();
+        s.nextLine();
+        Connection conn = db.getConn();
+
+        //Lock only the order table, to still allow selecting from the inquire products
+        String query =  " SELECT " +
+                        "       orderDate" +
+                        "     , requiredDate" +
+                        "     , shippedDate" +
+                        "     , customerNumber" +
+                        "     , status" +
+                        " FROM orders o " +
+                        " WHERE o.orderNumber = ?" +
+                        " FOR UPDATE";
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, orderID);
+
+        System.out.println("[Press ENTER key to start retrieving the data]");
+        s.nextLine();
+
+        ResultSet rs = pstmt.executeQuery();
+        String status="";
+        while (rs.next()) {
+            System.out.println("Order Information");
+            System.out.println("=====================");
+            System.out.println("Order Date:    " + rs.getDate("orderDate"));
+            System.out.println("Required Date: " + rs.getDate("requiredDate"));
+            status = rs.getString("status");
+            System.out.println("Status:        " + status);
+            Date shippedDate = rs.getDate("shippedDate");
+
+            if(shippedDate!=null)
+                System.out.println("Shipped Date:  " + shippedDate);
+
+            System.out.println("Customer Number: " + rs.getInt("customerNumber"));
+        }
+        rs.close();
+
+        System.out.println();
+        System.out.println("Order Details");
+        System.out.println("=====================");
+        System.out.println();
+
+        //Retrieve orders
+        query =     " SELECT " +
+                    "       productName" +
+                    "     , quantityOrdered" +
+                    "     , priceEach" +
+                    "     , orderLineNumber " +
+                    " FROM orderdetails od" +
+                    " LEFT JOIN products p ON od.productCode   = p.productCode" +
+                    " WHERE od.orderNumber = ?";
+        pstmt = conn.prepareStatement(query);
+        pstmt.setString(1, orderID);
+
+        rs = pstmt.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs.getString("productName"));
+            System.out.println("=====================");
+            System.out.println("Quantity:  " + rs.getInt("quantityOrdered"));
+            System.out.println("Individual Price:  " + rs.getFloat("priceEach"));
+            System.out.println("Order Line Number: " + rs.getInt("orderLineNumber"));
+            System.out.println();
+        }
+        rs.close();
+
+        if(status.equals("In Process")){
+            query = "UPDATE" +
+                    "     orders" +
+                    " SET" +
+                    "     status = 'Cancelled'" +
+                    " WHERE orderNumber = ?";
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, orderID);
+            System.out.println("[Press ENTER key to cancel the order]");
+            s.nextLine();
+
+            pstmt.executeUpdate();
+        }else
+            System.out.println("Can't cancel. Order has been " + status);
+
+        db.commit();
+        System.out.println("[Press ENTER key to end Transaction]");
+        s.nextLine();
+
+        pstmt.close();
+        db.closeConn();
     }
 
 }
